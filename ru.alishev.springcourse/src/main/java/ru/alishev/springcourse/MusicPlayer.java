b8e727bed99ff9d7cf532f044402bb870098a32a
beket <beketsk@gmail.com>
2021-08-19 14:51:48 +0600

    FEAT(qualifier): qualifier is for ukazat which class you use by kovychkas

--- ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,56 +1,21 @@
 package ru.alishev.springcourse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer  {
-
-    private QPOP qpop;
-    private ClassicalMusic classicalMusic;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, QPOP qpop) {
-        this.classicalMusic = classicalMusic;
-        this.qpop = qpop;
+    public MusicPlayer(@Qualifier("QPOP") Music music1, @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
-    //IoC зависит, так как просто музыка можно выбирать любую жанру музыки
-    //ioc
-//    private String name;
-//    private int volume;
-//
-//    public Integer getVolume() {
-//        return volume;
-//    }
-//    public void setVolume(Integer volume) {
-//        this.volume = volume;
-//    }
-//
-//    public String getName(){
-//        return name;
-//    }
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-
-//    public MusicPlayer(Music music) {
-//        this.music = music;
-//    }
-//
-//    @Autowired
-//    public void setMusic(Music music) {
-//        this.music = music;
-//    }
-//
-//    public MusicPlayer(){}
-//
-//    public void setMusic(Music music) {
-//        this.qpop = qpop;
-//    }
-
 
     public String playMusic() {
-        return "Playing: " + qpop.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
     }
 
 }
--- ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/QPOP.java
+++ ru.alishev.springcourse/src/main/java/ru/alishev/springcourse/QPOP.java
@@ -4,19 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class QPOP implements Music{
-//    private QPOP() {}
-//
-//    public static QPOP getqpop() {
-//        return new QPOP();
-//    }
-//
-//    public void doMyInit() {
-//        System.out.println("Doing my initialization");
-//    }
-//
-//    public void doMyDestroy() {
-//        System.out.println("Do my destructuib");
-//    }
     @Override
     public String getSong(){
         return "NinetyOne";
